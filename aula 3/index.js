1b47753ef6b53559e80d190c4949297edf878d6d
r4di0head <bocchiannajulia@gmail.com>
2024-08-13 15:32:54 -0300

    initial commit

---
+++ aula 3/index.js
@@ -0,0 +1,42 @@
+// texto exemplo
+
+ /*kl
+ af
+ */
+
+ console.log ("olá mundo")
+
+ console.log ("olá turma 24-2t")
+
+ console.log ("AAAAAAA")
+const nome = "fulana"
+const idade = 18
+ console.log("olá" , "meu nome é:", nome, "e eu tenho", idade, "anos")
+
+ const nome1 = "Anna"
+ const sobrenome = "bocchi"
+ const idade1 = 16
+ const funçao = "estudante"
+
+ 
+ console.log("olá" , "meu nome é:", nome1, sobrenome,"e eu tenho", idade1, "anos")
+
+ const got = "game of thrones"
+ const temporadasDeGot = "8";
+
+
+console.log (typeof got)
+console.log (typeof temporadasDeGot)
+
+const idadeNumero = 23
+const idadeTexto = idadeNumero.toString()
+console.log(idadeNumero)
+console.log(idadeTexto)
+
+const idadeTexto2 ="23"
+const idadeNumero2 = Number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeNumero2)
+
+
+
