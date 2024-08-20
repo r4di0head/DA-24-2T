048fa07f137c55307e6e773a80562de094071ea1
r4di0head <bocchiannajulia@gmail.com>
2024-08-20 16:52:19 -0300

    aula 6  e 7

---
+++ aula 6/index.js
@@ -0,0 +1,159 @@
+/*aula 6 operadores aritméticos
+
+                             Exemplo de adiçao
+
+const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor + segundoValor + 6
+console.log("o resultado é:",resultado) 
+
+                             Exemplo de subtraçao
+
+const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor - segundoValor
+console.log("o resultado é:",resultado) 
+ 
+                             Exemplo de multiplicaçao
+
+const primeiroValor = 10
+const segundoValor = 25
+const resultado = primeiroValor * segundoValor
+console.log("o resultado é:",resultado) 
+
+                            Exemplo de divisao 
+
+const primeiroValor = 345
+const segundoValor = 10
+const resultado = primeiroValor / segundoValor
+console.log("o resultado é:",resultado) 
+
+        Exemplo resto da divisao
+
+const restoDivisao = 11%4
+console.log("o resto da divisao é:",restoDivisao) 
+
+
+                                    EXERCICIOS
+1)
+const primeiroValor = 3
+const segundoValor = 4
+const resultado = primeiroValor + segundoValor
+console.log(resultado)
+ 
+resultado: 7
+
+2)
+const primeiroValor = 3
+const segundoValor = 5
+const resultado = primeiroValor * segundoValor / 2
+console.log(resultado) 
+
+resultado: 7,5  
+
+3) 
+const primeiroValor = 5
+const segundoValor = 4
+const resultado = primeiroValor - segundoValor * -1
+console.log(resultado)  
+
+resultado: 9  
+
+
+const restoDivisao = 11%4
+console.log(restoDivisao) 
+
+const primeiroValor = 234
+const segundoValor = 5
+const resultado = primeiroValor / segundoValor
+console.log(resultado)
+
+const restoDivisao = 46%8
+console.log(restoDivisao) 
+
+resultado = 6 
+
+/*                                 EXEMPLOS COMPARADORES                                                                                         
+
+                                     comparador igual
+
+/* const condiçao = 1 === 1
+console.log(condiçao)        resultado true
+
+const condiçao2 = 1 === 2
+console.log(condiçao2)       resultado false
+
+const condiçao3 = 1 === "1"          
+console.log(condiçao3)       resultado false 
+
+
+                                         comparador diferente
+
+const condiçao = 1 !== "1"
+console.log(condiçao)       resultado true
+
+const condiçao2 = 1 !== 1
+console.log(condiçao2)      resultado false
+
+const condiçao3 = 2 !== 1
+console.log(condiçao3)      resultado true 
+
+
+                                         maior, menor e maior e igual
+/*
+const condiçao = 1 > 2
+console.log(condiçao)     resultado: false
+
+const condiçao1 = 2 > 2
+console.log(condiçao1)    resultado false
+
+const condiçao2 = 2 >= 2
+console.log(condiçao2)    resultado true
+
+const condiçao3 = 3 > 2
+console.log(condiçao3)    resultado true
+
+
+                                       // comparador menor, menor e igual
+
+const condiçao = 1 < 2 
+console.log(condiçao) // resultado true
+const condiçao1 = 2 < 2 
+console.log(condiçao1) // resultado false
+const condiçao2 = 2 <= 2
+console.log(condiçao2) //resultado true
+const condiçao3 = 3 < 2
+console.log(condiçao3) //resultado false */
+
+                                   
+//                                 EXERCICIOS 
+
+//1 
+/*const primeiroNumero = 6
+const segundoNumero = 7
+const condiçao1 = primeiroNumero === segundoNumero
+console.log("o primeiro numero é igual ao segundo?",condiçao1)  //false
+
+//2 
+
+const condiçao2 = primeiroNumero !== segundoNumero 
+console.log("O primeiro numero é diferente do segundo?",condiçao2) //true
+
+//3
+const condiçao3 = primeiroNumero > segundoNumero
+console.log("O primeiro numero é maior que o segundo?",condiçao3) //false 
+
+//4
+const condiçao4 = primeiroNumero < segundoNumero
+console.log("O primeiro numero é menor do que o segundo?",condiçao4) //true */
+
+        /* let resultado = 100
+        resultado = resultado + 10
+        console.log(resultado)
+
+        let resultado1 = 100
+        resultado1 *= 10
+
+        console.log(resultado1)*/
+
+
