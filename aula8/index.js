6e4a527dee3692becaf2921017914a62e6140c77
Anna <bocchiannajulia@gmail.com>
2024-08-22 17:24:50 -0300

    Add files via upload

---
+++ aula8/index.js
@@ -0,0 +1,105 @@
+
+// AULA 8 CONCATENAÇAO
+
+/*const texto1 = "ola mundo"
+const texto2 = 'ola mundo'
+const texto3 = `ola mundo`
+
+const nome = "anna"
+const idade = 16 */
+
+
+//Exemplo concatenaçao
+/*const frase = "meu nome é: " + nome + " e minha idade é: " + idade*/
+
+
+//Exemplo template
+/*const frase = `meu nome é: ${nome} minha idade é: ${idade}`*/
+
+// exercicios
+/* const nome = prompt("seu nome?")
+const favCor = prompt("qual a sua cor favorita?") */
+
+// concatenaçao
+
+/* const frase = "seu nome é: " + nome + " e sua cor favorita é: " + favCor 
+
+console.log(frase) */
+
+
+//template
+
+/*const frase = `seu nome é: ${nome} e sua cor favorita é: ${favCor}`
+
+console.log(frase) */
+
+/*const nome = ` AwawaWAWAWAaw a `
+console.log(nome.length) //mostra quantas letras tem
+console.log(nome.toLowerCase()) //tira  caps
+console.log(nome.toUpperCase()) // aplica caps
+
+const email = "ajsdiafj@hotmail.com"
+console.log(email.trim()) //tira os espaços do começo e fim da frase
+
+const frase = "hoje comi bolo"
+console.log(frase.includes("bolo")) //true ou false se tem a palavra, se sim, respondera true
+console.log(frase.includes("lixo")) //mesmo porem com falso
+console.log(frase)
+
+const novaFrase = frase.replaceAll ("bolo","lixo")
+console.log(novaFrase) // troca*/
+
+//     EXERCICIO
+/*const frase = prompt("escreva uma frase")
+console.log(frase.toUpperCase())
+
+const novaFrase = frase.replaceAll ("o","i")
+
+console.log(novaFrase)
+
+console.log(frase.length)*/
+
+// ARRAYS, listas de elementos
+
+/*const frutas = ["abacate","banana","tomate","milho","uva","morango"] //comeca a contar com 0.
+
+const fruta = frutas[2]
+console.log(fruta) //tomate
+
+const fruta1 = frutas[4]
+console.log(fruta1) //uva */
+
+// Exercicio
+
+/*const cachorros =["labrador","pitbull","dalmata","golden","pug"]
+
+const cachorro = Number(prompt("escolha um numero de 0 a 4"))
+console.log(cachorros[cachorro]) */
+
+/* const pokemons = ["articuno", "moltres", "zapdos", "mewtwo"]
+console.log(pokemons.length)
+
+const seriesBoas = ["American Horror story", "saw", "Breaking bad"]
+
+console.log(seriesBoas.includes("saw"))
+console.log(seriesBoas.includes("Game of Thrones")) */
+
+/* const numeros = [1,2,3]
+numeros.push(4)
+console.log(numeros)
+numeros.push(5,6,7,8)
+console.log(numeros)
+
+
+const meusPeixes = ["palhaço",",mandarim","esturjao"]
+meusPeixes.pop()
+console.log(meusPeixes)
+
+const letras = ["A", "B", "C", "D", "E", "F", "G", "H"]
+letras.splice(2,1)
+console.log(letras)
+
+letras. splice(5,2)
+console.log(letras) */
+
+const numeros = [1,2,3,4,5,6]
