6d2c5854db481cd50cc81178ac70bf883a0bacdd
Anna <bocchiannajulia@gmail.com>
2024-09-05 17:17:27 -0300

    Add files via upload

---
+++ aula 14 exercicios/index.js
@@ -0,0 +1,78 @@
+//      Exercicios
+
+//    contador simples
+
+/* let contador = [1,2,3,4,5,6,7,8,9,10]
+for(let numero of contador){
+    console.log(numero) } */
+
+//    tabuada
+
+/* let numero = prompt("escolha um numero")
+for( let i= 1; i<= 10; i++){
+    console.log(numero + "x" + i + "=" + numero * i)
+} */
+
+//   soma de numeros
+
+/* let total = 0 
+for( let i = 1; i <= 5; i++){
+let numero = prompt("digite o numero" +i +":");
+total += Number (numero);
+}
+console.log("a soma total dos 5 numeros é:" + total) */
+
+//  media de notas
+
+/* let totalNotas = 0
+for(let i = 1; i <=5; i++){
+    let nota = prompt("digite a nota do aluno" + i + ":")
+    totalNotas +=  Number(nota);
+}
+
+let media = totalNotas / 5
+console.log("a media das notas dos 5 alunos é:" + media) */
+
+//  jogo da forca
+
+const palavras = ["gato" , "skate" , "praça" , "musica" , "jogo"]
+const gato = ["g", "a" , "t" , "o"]
+const skate = ["s","k","a","t","e"]
+const praca = ["p","r","a","ç","a"]
+const musica = ["m","u","s","i","c","a"]
+const jogo = ["j","o", "g", "o"]
+
+
+
+
+let palavraEscolhida = Math.floor(Math.random()*
+5)
+
+console.log (palavraEscolhida)
+
+
+while (palavraEscolhida === gato){
+
+
+}
+    console.log(gato)
+
+
+    /*
+} else if (palavraEscolhida === skate){
+    console.log(skate)
+} else if(palavraEscolhida === praca){
+    console.log(praca)
+}else if(palavraEscolhida === musica){
+    console.log(musica)
+}else if(palavraEscolhida === jogo){
+    console.log(jogo)
+} else {
+    console.log("nuh uh")
+}*/
+
+
+//validador de cpf
+
+
+
