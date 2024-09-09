48149a9798f9ec5851228701e1306a2c4c2e282e
Anna <bocchiannajulia@gmail.com>
2024-09-09 17:07:40 -0300

    Add files via upload

---
+++ aula 15 funcoes/index.js
@@ -0,0 +1,151 @@
+/* function calculaArea(largura, altura){
+const area = largura * altura
+console.log(area)
+
+}
+calculaArea(2,3)
+calculaArea(52,49)
+calculaArea(1235132, 75352)
+calculaArea( -235, 167)
+
+function imprimirOlaMundo(){
+    console.log("Ola mundo")
+}
+imprimirOlaMundo()
+
+
+/* function imprimirNome (nome){
+console.log("Bem vindo,:", nome)
+}
+imprimirNome("Rebeca")
+imprimirNome("Joao")*/
+
+// exercicio 1
+/*function imprimir (turma){
+    console.log("Bem vindo turma", turma ,"!")
+    }
+    imprimir("24-2T")
+
+// exercicio 2
+    function calcular(num1, num2){
+const resultado= num1 + num2
+console.log("A soma é:", resultado)
+
+}
+calcular(3,8)
+
+ // exercicio 3
+/* function dobrar(numero){
+    const dobro = numero * numero
+    console.log("O dobro é:", dobro)
+ }
+dobro(3) */
+ 
+
+//exercicio 3
+/*function imprimirNome (nome){
+    console.log(`Olá ${nome}`)
+    }
+    imprimirNome("gabriel")
+    imprimirNome("gato")
+    imprimirNome("anna")
+
+//exercicio 4
+function calcular(num1, num2){
+    const resultado= num1 + num2
+    console.log(`A soma é:${resultado}`)
+}    
+
+calcular(5,4)
+calcular(8,12)
+
+//exercicio 5
+function imprimeNome(nome){
+    console.log("salve", nome)
+}
+
+imprimeNome("Vitória") */
+
+
+       // exemplo return */
+
+     /*  function calculaArea(largura, altura){
+        const resultadoArea = largura * altura
+        return resultadoArea
+       }
+
+       let area = calculaArea(2,5)
+       console.log("A area é:", area)
+       console.log("A area é:", calculaArea (2,3)) */
+
+// exercicio 1
+
+
+/* function somaNumeros(num1,num2){
+
+    const soma = num1+num2
+    return soma
+}
+let resultado = somaNumeros(2,5)
+  console.log("a  soma é:", somaNumeros(2,5)) 
+
+  const numerosOriginal = [243, 235, 622, 23, 4, 6]
+  function primeiroUltimo (arrayNumeros) {
+  const novoArray = []
+  novoArray.push(arrayNumeros[0])
+  novoArray.push(arrayNumeros[arrayNumeros.length-1])
+  return novoArray
+}
+console.log(primeiroUltimo(numerosOriginal)) */
+
+
+// exemplo
+/* const calculaArea = function(largura, altura){
+
+    const soma = largura * altura
+    return soma
+}
+const resultado = calculaArea(2,5)
+
+
+
+// e2
+    const imprimeNome = function(nome){
+        console.log("bem vindo:", nome)
+    }
+    imprimeNome("gab") */
+
+
+    // arrow functions
+
+  /* const calculaArea = (altura, largura) => {
+        const resultado = largura * altura
+        return resultado
+    }
+    const area = calculaArea(2,7)
+
+    console.log("a area é:", area) */
+
+
+    // exercicio 8
+/* const imprimir = function(turma){
+    console.log("bem vindo turma", turma, "!")
+}
+
+
+// exercicio 9
+
+const calcular = (num1,num2) => {
+    const resultado = num1 + num2
+    return resultado
+}
+
+const soma = resultado(3,2)
+console.log("a soma é:", soma) */
+
+function determinaMonstro (monstro){
+    
+ }
+ 
+ 
+ imprimir("24-2T")
