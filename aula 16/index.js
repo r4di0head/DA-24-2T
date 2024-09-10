d36fc55850b9e7131347929865ca7a2135333371
r4di0head <bocchiannajulia@gmail.com>
2024-09-10 16:28:30 -0300

    aula 16

---
+++ aula 16/index.js
@@ -0,0 +1,70 @@
+// interpretaçao de codigo 1
+
+/* function minhaFunçao(variavel){
+    return variavel * 5 
+}          //a funçao ia fazer qualquer numero dentro do () multiplicado por 5
+console.log(minhaFunçao(2))
+console.log(minhaFunçao(10)) */
+
+
+// interpretaçao de codigo 2
+
+/* let textoUsuario = prompt("insira um texto"); //pegando a reposta do usuario
+const outraFuncao = function(texto){ //criando a funçao para a resposta
+
+    return texto.toLowerCase().includes("cenoura") //retorna a resposta com todas as letras minusculas e diz se há "cenoura" no texto
+}
+const resposta = outraFuncao (textoUsuario)  //
+console.log(resposta) // imprimindo */
+
+//b. 1 = apenas aparecera que o texto inclui cenoura
+//2 = o CENOURA ficara em minusculo
+//3 = nao aparecerá pois cenoura esta no plural
+
+// escrita de código 1
+
+/* function imprimirNome (nome){
+    console.log("eu sou", nome)
+    }
+    imprimirNome("anna")
+
+    function imprimirIdade (idade){
+        console.log("eu tenho", idade, "anos")
+        }
+        imprimirIdade("16")
+
+     function imprimirlocal (local){
+            console.log("moro em", local)
+            }
+            imprimirlocal("sapucaia")
+
+            function imprimirEsc (esc){
+                console.log("e sou", esc)
+                }
+                imprimirEsc("estudante") */
+
+// escrita de código 2
+
+/* function adiçao (soma){
+    prompt("digite 2 numeros")
+    }
+    adiçao()
+
+ function subtraçao (menos){
+    prompt("digite 2 numeros")
+        console.log()
+        }
+      subtraçao()
+
+ function multiplicado (vezes){
+    prompt("digite 2 numeros")
+            console.log()
+            }
+           multiplicado()
+
+function divisao (divide){
+    prompt("digite 2 numeros")
+                console.log()
+                }
+        divisao()
+*/
